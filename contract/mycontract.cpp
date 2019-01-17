1ad4d2d575ea069748ff27c85dbb01f82fb9b3c7
Aren Chen <yarencheng@gmail.com>
2019-01-16 18:36:41 -0800

    contract

---
+++ contract/mycontract.cpp
@@ -0,0 +1,17 @@
+#include "mycontract.hpp"
+
+using namespace ::std;
+using namespace ::eosio;
+using namespace ::mycontract;
+
+MyContract::MyContract(eosio::name receiver, eosio::name code, eosio::datastream<const char *> ds)
+    : contract(receiver, code, ds),
+      helloWorld(receiver, code.value)
+{
+}
+
+void MyContract::hi(name user)
+{
+    auto hw = helloWorld.get_or_create(_self);
+    print("Message: ", hw.echo(user.to_string()));
+}
---
+++ contract/mycontract.hpp
@@ -0,0 +1,22 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/singleton.hpp>
+#include "hello_world.hpp"
+
+namespace mycontract
+{
+
+class MyContract : public eosio::contract
+{
+  public:
+    using eosio::contract::contract;
+
+    MyContract(eosio::name receiver, eosio::name code, eosio::datastream<const char *> ds);
+
+    [[eosio::action]] void hi(eosio::name user);
+
+    eosio::singleton<"helloworld"_n, mylib::HelloWorld> helloWorld;
+};
+
+EOSIO_DISPATCH(MyContract, (hi))
+
+} // namespace mycontract
